a00a68fb3b4cbb70cd8a19e50ff020efe4f5fe23
5hamele55 <59293323+5hamele55@users.noreply.github.com>
2020-02-14 15:55:48 +0200

    Update main.js

--- main.js
+++ main.js
@@ -16,22 +16,155 @@ let sum1 = document.getElementById('sum1');
 let upR1c1 = 0;
 let upR1c2 = 0;
 let upR1c3 = 0;
+let r2c1 = document.getElementById('r2c1');
+let r2c2 = document.getElementById('r2c2');
+let r2c3 = document.getElementById('r2c3');
+let sum2 = document.getElementById('sum2');
+let upR2c1 = 0;
+let upR2c2 = 0;
+let upR2c3 = 0;
+let r3c1 = document.getElementById('r3c1');
+let r3c2 = document.getElementById('r3c2');
+let r3c3 = document.getElementById('r3c3');
+let sum3 = document.getElementById('sum3');
+let upR3c1 = 0;
+let upR3c2 = 0;
+let upR3c3 = 0;
+
+//radio buttons changes calculations
+let radioSum = document.getElementById('radioSum');
+
+radioSum.addEventListener('input', (event)=> {
+    if (event.target.checked) updateValue1(null, 'add'),
+    updateValue4(null, 'add'),
+    updateValue7(null, 'add')
+});
+
+let radioMultiply = document.getElementById('radioMultiply');
+
+radioMultiply.addEventListener('input', (event)=> {
+    if (event.target.checked) updateValue1(null, 'multiply'),
+    updateValue4(null, 'multiply'),
+    updateValue7(null, 'multiply')
+});
+
 r1c1.addEventListener('input', updateValue1);
 function updateValue1(e) {
+    let action = 'add';
+    if (radioMultiply.checked) {
+        action = 'multiply';
+    }
+
     if (e){
-    upR1c1 = e.target.value;
-}
-    sum1.innerHTML = calc(upR1c1, upR1c2, upR1c3);
+        upR1c1 = e.target.value;
+    }
+
+    sum1.innerHTML = calc(action, upR1c1, upR1c2, upR1c3);
   }
 r1c2.addEventListener('input', updateValue2);
 function updateValue2(e) {
-    upR1c2 = e.target.value;
-    sum1.innerHTML = calc(upR1c1, upR1c2, upR1c3);
+    let action = 'add';
+    if (radioMultiply.checked) {
+        action = 'multiply';
+    }
+
+    if (e){
+        upR1c2 = e.target.value;
+    }
+
+    sum1.innerHTML = calc(action, upR1c1, upR1c2, upR1c3);
   }
+
 r1c3.addEventListener('input', updateValue3);
 function updateValue3(e) {
-    upR1c3 = e.target.value;
-    sum1.innerHTML = calc(upR1c1, upR1c2, upR1c3);
+    let action = 'add';
+    if (radioMultiply.checked) {
+        action = 'multiply';
+    }
+
+    if (e){
+        upR1c3 = e.target.value;
+    }
+
+    sum1.innerHTML = calc(action, upR1c1, upR1c2, upR1c3);
+}
+r2c1.addEventListener('input', updateValue4);
+function updateValue4(e) {
+    let action = 'add';
+    if (radioMultiply.checked) {
+        action = 'multiply';
+    }
+
+    if (e){
+        upR2c1 = e.target.value;
+    }
+
+    sum2.innerHTML = calc(action, upR2c1, upR2c2, upR2c3);
+  }
+  r2c2.addEventListener('input', updateValue5);
+function updateValue5(e) {
+    let action = 'add';
+    if (radioMultiply.checked) {
+        action = 'multiply';
+    }
+
+    if (e){
+        upR2c2 = e.target.value;
+    }
+
+    sum2.innerHTML = calc(action, upR2c1, upR2c2, upR2c3);
+  }
+  r2c3.addEventListener('input', updateValue6);
+function updateValue6(e) {
+    let action = 'add';
+    if (radioMultiply.checked) {
+        action = 'multiply';
+    }
+
+    if (e){
+        upR2c3 = e.target.value;
+    }
+
+    sum2.innerHTML = calc(action, upR2c1, upR2c2, upR2c3);
+  }
+  r3c1.addEventListener('input', updateValue7);
+function updateValue7(e) {
+    let action = 'add';
+    if (radioMultiply.checked) {
+        action = 'multiply';
+    }
+
+    if (e){
+        upR3c1 = e.target.value;
+    }
+
+    sum3.innerHTML = calc(action, upR3c1, upR3c2, upR3c3);
+  }
+  r3c2.addEventListener('input', updateValue8);
+function updateValue8(e) {
+    let action = 'add';
+    if (radioMultiply.checked) {
+        action = 'multiply';
+    }
+
+    if (e){
+        upR3c2 = e.target.value;
+    }
+
+    sum3.innerHTML = calc(action, upR3c1, upR3c2, upR3c3);
+  }
+  r3c3.addEventListener('input', updateValue9);
+function updateValue9(e) {
+    let action = 'add';
+    if (radioMultiply.checked) {
+        action = 'multiply';
+    }
+
+    if (e){
+        upR3c3 = e.target.value;
+    }
+
+    sum3.innerHTML = calc(action, upR3c1, upR3c2, upR3c3);
   }
 // checkbox chagne colors of rows
 let box1 = document.getElementById('box1');
@@ -40,6 +173,7 @@ let box3 = document.getElementById('box3');
 let container1 = document.getElementById('r1');
 let container2 = document.getElementById('r2');
 let container3 = document.getElementById('r3');
+
 function changeColor1 () {
     if (box1.checked) {
         container1.style.backgroundColor = "rgb(0, 205, 70)";
@@ -64,19 +198,14 @@ function changeColor3 () {
         container3.style.backgroundColor = "white";
     }
 }
-//radio buttons changes calculations
-let radioSum = document.getElementById('radioSum');
-radioSum.addEventListener('click', (a)=> {
-    console.log(radioSum.value.target.checked, a);
-    if (radioSum.checked) updateValue1()
-});
-let radioMultiply = document.getElementById('radioMultiply');
-radioMultiply.addEventListener('click', calc);
-function calc(n1, n2, n3) {
-    if (radioSum.checked) {
+
+
+function calc(action = 'add', n1, n2, n3) {
+    console.error(action, action === 'multiply', n1, n2, n3)
+    if (action === 'add') {
             return +n1 + +n2 + +n3;
         }
-    else {
+    if (action === 'multiply') {
             return +n1 * +n2 * +n3;
     }
  }
